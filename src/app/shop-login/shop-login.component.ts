af5b7e3c7647ba19a3d6c7531a1e429bbde72e13
Truong An <79464700+anduu156@users.noreply.github.com>
2021-10-07 19:21:22 +0700

    sso login facebook and google

--- src/app/shop-login/shop-login.component.ts
+++ src/app/shop-login/shop-login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SocialAuthService, GoogleLoginProvider, SocialUser, FacebookLoginProvider } from 'angularx-social-login';
 
 @Component({
   selector: 'app-shop-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-login.component.css']
 })
 export class ShopLoginComponent implements OnInit {
+  myForm!: FormGroup;
+  user!: SocialUser;
+  isSignedin!: boolean;
+  
+  constructor(
+    private formBuilder: FormBuilder, 
+    private socialAuthService: SocialAuthService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });    
+    
+    this.socialAuthService.authState.subscribe((user) => {
+      this.user = user;
+      this.isSignedin = (user != null);
+      console.log(this.user);
+    });
+  }
 
-  ngOnInit(): void {
+  facebookSignin(): void {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  logOut(): void {
+    this.socialAuthService.signOut();
   }
 
 }
